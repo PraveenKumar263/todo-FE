26d82de7f5e442e559a11031eb36743d5b9c3c0d
PraveenKumar263 <PraveenKumar263>
2024-08-24 14:31:36 +0800

    checkbox feature for the todo items with sort

--- src/components/ViewTodo.jsx
+++ src/components/ViewTodo.jsx
@@ -1,10 +1,13 @@
 import { useDispatch } from 'react-redux';
 import { useLoaderData } from 'react-router-dom';
 import { setIsEditing, setisEditingId, setNewTodo, setStatus } from '../features/todoSlice';
+import './ViewTodo.css';
+import todoServices from "../services/todoServices";
 
 const ViewTodo = () => {
     const todos = useLoaderData();
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleTodoClick = (todo) => {
         dispatch(setNewTodo(todo.description));
@@ -12,14 +15,43 @@ const ViewTodo = () => {
         dispatch(setIsEditing(true));
         dispatch(setisEditingId(todo._id));
     }
+
+    const handleCheck = (todo) => {
+        const confirm = window.confirm('Are you sure want to change the status of this todo?');
+        if(confirm) {
+            dispatch(setStatus(!todo.status));
+            todoServices.putTodo({
+                description: todo.description,
+                status: !todo.status
+            }, todo._id)
+            .then(() => {
+                alert('Todo updated');
+
+                navigate('/', { replace: true });
+            })
+            .catch(error => {
+                console.error(error);
+            })
+        }
+    }
+
     return (
         <>
             <h1>todos</h1>
-            <ul>
+            <ul className='todoList'>
             {
-                todos.map(todo => (
+                todos
+                .sort((a, b) => a.status < b.status)
+                .map(todo => (
                 <li key={todo._id}>
-                    <span onClick={() => handleTodoClick(todo)}>{todo.description}</span>
+                    <input type="checkbox" checked={todo.status} 
+                        onChange={() => handleCheck(todo)}
+                    />
+                    <span onClick={() => handleTodoClick(todo)}
+                        style={{ textDecoration: todo.status ? 'line-through': 'none'}}
+                        >
+                        {todo.description}
+                    </span>
                 </li>
                 ))
             }
